cc468c82b92d9ab0c7adf6d672ae00615a3dfc31
王 若凝 <3303357752@qq.com>
2024-11-07 20:54:20 +0800

    0.3 member模块测试

--- member/src/main/java/org/wrn/member/MemberApplication.java
+++ member/src/main/java/org/wrn/member/MemberApplication.java
@@ -1,13 +1,20 @@
 package org.wrn.member;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@ComponentScan("org.wrn")
 public class MemberApplication {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(MemberApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment environment = app.run(args).getEnvironment();
+        LOGGER.info("member 启动成功");
+        LOGGER.info("测试地址: \thttp://127.0.0.1:{}{}/hello", environment.getProperty("server.port"), environment.getProperty("server.servlet.context-path"));
     }
-
 }
---
+++ member/src/main/java/org/wrn/member/controller/TestController.java
@@ -0,0 +1,20 @@
+package org.wrn.member.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: Admin
+ * @Desc:
+ * @create: 2024-11-07 20:51
+ **/
+
+@RestController
+public class TestController {
+
+
+    @GetMapping("/hello")
+    public String test() {
+        return "test";
+    }
+}
