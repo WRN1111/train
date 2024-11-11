0cf8c375f86502f7a54ebee52fc0a1ef898091cf
王 若凝 <3303357752@qq.com>
2024-11-11 16:36:08 +0800

    0.4 增加全局controller日志打印

---
+++ common/src/main/java/org/wrn/common/aspect/LogAspect.java
@@ -0,0 +1,105 @@
+package org.wrn.common.aspect;
+
+import cn.hutool.core.util.RandomUtil;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.support.spring.PropertyPreFilters;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.aspectj.apache.bcel.generic.LOOKUPSWITCH;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author: Admin
+ * @Desc:
+ * @create: 2024-11-11 16:12
+ **/
+
+@Aspect
+@Component
+public class LogAspect {
+    LogAspect() {
+        System.out.println("LogAspect");
+    }
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(LogAspect.class);
+
+    @Pointcut("execution(public * org.wrn..*Controller.*(..))")
+    public void controllerPointcut() {
+    }
+
+    @Before("controllerPointcut()")
+    public void doBefore(JoinPoint joinPoint) {
+        // 增加日志流水号
+        MDC.put("LOG_ID", System.currentTimeMillis() + RandomUtil.randomString(3));
+
+
+        // 开始打印请求日志
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = null;
+        if (attributes != null) {
+            request = attributes.getRequest();
+        }
+        Signature signature = joinPoint.getSignature();
+        String name = signature.getName();
+
+        // 打印请求信息
+        LOGGER.info("------------- 开始 -------------");
+        if (request != null) {
+            LOGGER.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
+        }
+        LOGGER.info("类名方法: {}.{}", signature.getDeclaringTypeName(), name);
+        if (request != null) {
+            LOGGER.info("远程地址: {}", request.getRemoteAddr());
+        }
+
+        // 打印请求参数
+        Object[] args = joinPoint.getArgs();
+        LOGGER.info("请求参数: {}", JSONObject.toJSONString(args));
+
+        // 排除特殊类型的参数，如文件类型
+        Object[] arguments = new Object[args.length];
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] instanceof ServletRequest
+                    || args[i] instanceof ServletResponse
+                    || args[i] instanceof MultipartFile) {
+                continue;
+            }
+            arguments[i] = args[i];
+        }
+
+        // 排除字段，敏感字段或太长的字段不显示：身份证、手机号、邮箱、密码等
+        String[] excludeProperties = {"mobile"};
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
+        excludefilter.addExcludes(excludeProperties);
+        LOGGER.info("请求参数: {}", JSONObject.toJSONString(arguments, excludefilter));
+    }
+
+    @Around("controllerPointcut()")
+    public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object result = proceedingJoinPoint.proceed();
+        // 排除字段，敏感字段或太长的字段不显示：身份证、手机号、邮箱、密码等
+        String[] excludeProperties = {"mobile"};
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
+        excludefilter.addExcludes(excludeProperties);
+        LOGGER.info("返回结果: {}", JSONObject.toJSONString(result, excludefilter));
+        LOGGER.info("------------- 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
+        return result;
+    }
+}
