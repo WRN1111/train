a791923e01e4b0872aa2db32c11cb20d29d1fe7d
王 若凝 <3303357752@qq.com>
2024-11-12 21:13:51 +0800

    0.8 生成member和pass

---
+++ generator/src/main/java/org/wrn/train/generator/GeneatorApplication.java
@@ -0,0 +1,13 @@
+package org.wrn.train.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneatorApplication.class, args);
+    }
+
+}
---
+++ generator/src/test/java/org/wrn/train/generator/GeneatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wrn.train.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
